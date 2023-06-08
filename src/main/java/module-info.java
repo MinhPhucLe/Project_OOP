8da4cfe266817c2761478a83611046c59d1d282c
Phan Khoi Nguyen 20210652 <khoinguyen6103@gmail.com>
2023-06-09 00:57:22 +0700

    add module dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.demo2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jsoup;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.demo2 to javafx.fxml;
